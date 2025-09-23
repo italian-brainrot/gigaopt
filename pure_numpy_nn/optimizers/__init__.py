7a379a730464ae6a275f2579deae917a6a5ee8f1
italian-brainrot <95920368+italian-brainrot@users.noreply.github.com>
2025-09-23 22:58:45 +0300

    Update __init__.py

--- pure_numpy_nn/optimizers/__init__.py
+++ pure_numpy_nn/optimizers/__init__.py
@@ -1,10 +1,3 @@
 from .base import Optimizer
 from .momentum import Momentum
 from .adam import Adam
-from .ada_third import AdaThird
-from .nova import Nova
-from .adathirdv2 import AdaThirdV2
-from .cognio import CogniO
-from .cognitive_dissonance import CognitiveDissonanceOptimizer
-
-__all__ = ['Optimizer', 'Momentum', 'Adam', 'AdaThird', 'Nova', 'AdaThirdV2', 'CogniO', 'CognitiveDissonanceOptimizer']
