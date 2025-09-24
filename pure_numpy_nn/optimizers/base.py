5ddd1f8467ede1ca80ca0d49dfb8dc04d5b847fa
italian-brainrot <95920368+italian-brainrot@users.noreply.github.com>
2025-09-24 09:41:22 +0300

    Update base.py

--- pure_numpy_nn/optimizers/base.py
+++ pure_numpy_nn/optimizers/base.py
@@ -15,5 +15,19 @@ class Optimizer:
             closure (callable): 
                 A closure with a `backward` argument that reevaluates the model and returns (loss, gradients).
                 If `backward=False`, gradients are not computed and returned as None.
+
+        Closure example:
+        ```py
+        def closure(backward=True):
+            y_pred = net.forward(x_batch)
+            loss = mse_loss(y_batch, y_pred)
+            grads = None
+        
+            if backward:
+                loss_grad = mse_loss_derivative(y_batch, y_pred)
+                grads = net.backward(loss_grad)
+        
+            return loss, grads
+        ```
         """
         raise NotImplementedError
